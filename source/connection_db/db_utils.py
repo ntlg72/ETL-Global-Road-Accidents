d50f3294047c63435a7e9b215cd4e4f8b9c4be05
Natalia López Gallego <166937483+ntlg72@users.noreply.github.com>
2025-03-24 18:58:33 -0500

    Ignore all __pycache__ directories

---
+++ source/connection_db/db_utils.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+
+def get_connection():
+    load_dotenv()
+    user = os.getenv('PG_USER')
+    password = os.getenv('PG_PASSWORD')
+    host = os.getenv('PG_HOST')
+    port = os.getenv('PG_PORT')
+    dbname = os.getenv('PG_DATABASE')
+
+    
+    db_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
+
+    try:
+        engine = create_engine(db_url)
+        print("Engine creado exitosamente")
+        return engine
+    except Exception as e:
+        print(f"Error al crear el engine: {e}")
+        return None
+
+def close_connection(engine):
+    if engine:
+        try:
+            engine.dispose()
+            print("Conexión al Engine cerrada.")
+        except Exception as e:
+            print(f"Error al cerrar la conexión: {e}")
+    else:
+        print("No engine to close.")
